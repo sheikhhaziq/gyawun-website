ef429ba613108b2246fb4893fc218b8e253ea218
Sheikh Haziq <sheikhhaziq520@gmail.com>
2024-07-23 23:21:34 +0530

    addes sitemap

---
+++ next-sitemap.config.js
@@ -0,0 +1,9 @@
+/** @type {import('next-sitemap').IConfig} */
+const config = {
+    siteUrl: 'https://gyawunmusic.vercel.app/',
+    generateRobotsTxt: true,
+    // ...other options
+  }
+  
+  module.exports = config
+  
